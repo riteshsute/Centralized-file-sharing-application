afdbd398fb11e776d63130e839980a8a23818ce4
riteshsute <suteritesh@gmail.com>
2025-01-16 00:44:55 +0530

    authenticating

--- utils/authentication.js
+++ utils/authentication.js
@@ -5,6 +5,7 @@ const authenticate = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(' ')[1]; // Extract token from Authorization header
 
+    console.log(token)
     if (!token) {
       return res.status(401).json({ success: false, message: 'Authentication failed: No token provided' });
     }
